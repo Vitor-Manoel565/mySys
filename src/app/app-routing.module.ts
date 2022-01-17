b85563967af3d8bbe04006d9e34bfe172b2b04ff
Vitor Manoel <vitoralvesbvgs@gmail.com>
2022-01-17 16:24:30 -0300

    Primeiro commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'gastos',
+    loadChildren: () => import('./gastos/gastos/gastos.module').then( m => m.GastosPageModule)
+  },
+  {
+    path: 'client',
+    loadChildren: () => import('./client/client/client.module').then( m => m.ClientPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
